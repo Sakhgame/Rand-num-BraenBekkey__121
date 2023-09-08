0708588c1fe314e67ea30ce7563aa6b70f1bf010
Braen Bekkey <ikharitonov2016@gmail.com>
2023-09-08 11:36:06 +0700

    Добавьте файлы проекта.

---
+++ Rand-num-BraenBekkey__121/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите диапозон рандомного числа начиная с меньшего и заканивая пределом: ");
+int s = int.Parse(Console.ReadLine());
+int e = int.Parse(Console.ReadLine());
+Random rnd = new Random();
+int val = rnd.Next(s,e);
+Console.WriteLine(val);
